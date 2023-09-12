06fd9b5f601543e684b07bfba22c0544c28286ce
Ed Stifter <edstifter@gmail.com>
2023-09-12 13:06:26 -0400

    Modify test to include thing_name

--- aws.greengrass.labs.telemetry.InfluxDBPublisher/test/test_streamHandlers.py
+++ aws.greengrass.labs.telemetry.InfluxDBPublisher/test/test_streamHandlers.py
@@ -10,26 +10,25 @@ import src.streamHandlers as streamHandler
 from awsiot.greengrasscoreipc.model import (
     JsonMessage,
     BinaryMessage,
-    SubscriptionResponseMessage
+    SubscriptionResponseMessage,
 )
 
 sys.path.append("src/")
 
 testparams = {
-    'InfluxDBContainerName': 'greengrass_InfluxDB',
-    'InfluxDBOrg': 'greengrass',
-    'InfluxDBBucket': 'greengrass-telemetry',
-    'InfluxDBPort': '8086',
-    'InfluxDBInterface': '127.0.0.1',
-    'InfluxDBToken': 'vb53ZyYlxJjAeWcDbgPjbNvkvdD95b2hCrt0CoaZyEL5QYBiQfLw3TbgqgDozj74_aZ9pYCwVJM6Vj5quLAfSA==',
-    'InfluxDBServerProtocol': 'https',
-    'InfluxDBSkipTLSVerify': 'true',
-    'InfluxDBTokenAccessType': 'RW'
+    "InfluxDBContainerName": "greengrass_InfluxDB",
+    "InfluxDBOrg": "greengrass",
+    "InfluxDBBucket": "greengrass-telemetry",
+    "InfluxDBPort": "8086",
+    "InfluxDBInterface": "127.0.0.1",
+    "InfluxDBToken": "vb53ZyYlxJjAeWcDbgPjbNvkvdD95b2hCrt0CoaZyEL5QYBiQfLw3TbgqgDozj74_aZ9pYCwVJM6Vj5quLAfSA==",
+    "InfluxDBServerProtocol": "https",
+    "InfluxDBSkipTLSVerify": "true",
+    "InfluxDBTokenAccessType": "RW",
 }
 
 
 def test_validInfluxDBParams(mocker):
-
     handler = streamHandler.InfluxDBDataStreamHandler()
     message = JsonMessage(message=testparams)
     response_message = SubscriptionResponseMessage(json_message=message)
@@ -50,15 +49,38 @@ def test_invalidInfluxDBParams(mocker):
         assert pytest_wrapped_e.type == SystemExit
 
 
-@patch('influxdb_client.InfluxDBClient')
+@patch("influxdb_client.InfluxDBClient")
 def test_valid_telemetry_received(InfluxDBClient, mocker):
-
     client = InfluxDBClient("http://localhost", "my-token", org="my-org", debug=True)
 
     testTelemetry = [
-        {"A": "Average", "N": "CpuUsage", "NS": "SystemMetrics", "TS": 1627597331445, "U": "Percent", "V": 26.21981271562346},
-        {"A": "Count", "N": "TotalNumberOfFDs", "NS": "SystemMetrics", "TS": 1627597331445, "U": "Count", "V": 7316},
-        {"A": "Count", "N": "SystemMemUsage", "NS": "SystemMetrics", "TS": 1627597331445, "U": "Megabytes", "V": 10098}
+        {
+            "A": "Average",
+            "N": "CpuUsage",
+            "NS": "SystemMetrics",
+            "TS": 1627597331445,
+            "U": "Percent",
+            "V": 26.21981271562346,
+            "thing_name": "thing_name",
+        },
+        {
+            "A": "Count",
+            "N": "TotalNumberOfFDs",
+            "NS": "SystemMetrics",
+            "TS": 1627597331445,
+            "U": "Count",
+            "V": 7316,
+            "thing_name": "thing_name",
+        },
+        {
+            "A": "Count",
+            "N": "SystemMemUsage",
+            "NS": "SystemMetrics",
+            "TS": 1627597331445,
+            "U": "Megabytes",
+            "V": 10098,
+            "thing_name": "thing_name",
+        },
     ]
 
     handler = streamHandler.TelemetryStreamHandler(testparams)
@@ -68,7 +90,8 @@ def test_valid_telemetry_received(InfluxDBClient, mocker):
 
     assert client.write_api.called
     handler.write_client.write.assert_called_with(
-        bucket=testparams['InfluxDBBucket'], org=testparams['InfluxDBOrg'], record=ANY)
+        bucket=testparams["InfluxDBBucket"], org=testparams["InfluxDBOrg"], record=ANY
+    )
 
 
 def test_none_telemetry_received():
