2089bc504212cd9efff9b7633d11b93fbc999ef0
Ed Stifter <estifter@amazon.com>
2023-09-19 12:35:33 -0400

    remove SNS debugging because it will not work offline

--- com.offline.Alarm/alarm.py
+++ com.offline.Alarm/alarm.py
@@ -3,13 +3,11 @@ import time
 from argparse import ArgumentParser
 import subprocess
 
-import boto3
 from awsiot.greengrasscoreipc.clientv2 import GreengrassCoreIPCClientV2
 from awsiot.greengrasscoreipc.model import SubscriptionResponseMessage
 
 
 parser = ArgumentParser(__name__)
-parser.add_argument("--sns_topic", required=True)
 parser.add_argument("--on_alarm_command")
 
 telemetry_topic = "injected/greengrass/telemetry"
@@ -17,13 +15,7 @@ telemetry_topic = "injected/greengrass/telemetry"
 already_alarmed = set()
 
 
-def send_to_sns(client, topic_arn: str, message: str):
-    client.publish(TopicArn=topic_arn, Message=message)
-
-
-def check_telemetry(
-    ipc_client: GreengrassCoreIPCClientV2, boto_client, topic_arn, on_alarm_command
-):
+def check_telemetry(ipc_client: GreengrassCoreIPCClientV2, on_alarm_command):
     def on_tel_event(e: SubscriptionResponseMessage):
         telemetry_data = None
         if e.binary_message and e.binary_message.message:
@@ -44,24 +36,13 @@ def check_telemetry(
                     {"stdout": proc.stdout.decode(), "stderr": proc.stderr.decode()},
                 )
 
-            message = {
-                "source": "com.offline.Alarm",
-                "components_broken": list(broken_components),
-            }
-            print(f"sending message: {message}")
-            send_to_sns(boto_client, topic_arn, json.dumps(message))
-
     ipc_client.subscribe_to_topic(topic=telemetry_topic, on_stream_event=on_tel_event)
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    client = boto3.client("sns")
-    topic_arn = args.sns_topic
 
-    check_telemetry(
-        GreengrassCoreIPCClientV2(), client, topic_arn, args.on_alarm_command
-    )
+    check_telemetry(GreengrassCoreIPCClientV2(), args.on_alarm_command)
     timer = 0
     while True:
         # every 5 minutes we reset the alarm
